ed3e5da6a9074bc5346cb520cbebbe36597777f6
Debra Winter <deb@debwinter.com>
2024-01-15 16:11:20 -0500

    exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+            int favNumber = r.Next(1, 101);
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+            int userGuess = Guess();
+            
+            do
+            {
+                Console.WriteLine(Evaluate(userGuess, favNumber));
+                userGuess = Guess();
+            } while (userGuess != favNumber);
+
+            Console.WriteLine($"You got it! I was thinking of the number {favNumber}.");
+        }
+
+        public static int Guess()
+        {
+            Console.Write("What number am I thinking of? ");
+            int guess = int.Parse(Console.ReadLine());
+            return guess;
+        }
+
+        public static string Evaluate(int userGuess, int actualNumber)
+        {
+            bool tooLow = userGuess < actualNumber;
+            bool tooHigh = userGuess > actualNumber;
+
+            string message;
+
+            if (tooLow)
+            {
+                message = "Too low! Try again:";
+            }
+            else if (tooHigh)
+            {
+                message = "Too high! Try again:";
+            }
+            else
+            {
+                message = "Sorry, I didn't understand. Try again:";
+            }
+            return message;
         }
     }
 }
